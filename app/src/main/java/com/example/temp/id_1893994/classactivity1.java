5f04df668cfea4b6db833602eb1cf34848aebeee
vinay <vinayraodugyala99@gmail.com>
2019-06-03 16:52:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/temp/id_1893994/classactivity1.java
@@ -0,0 +1,13 @@
+package com.example.temp.id_1893994;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class classactivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classactivity1);
+    }
+}
